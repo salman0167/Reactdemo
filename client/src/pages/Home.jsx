d881be60a4dba12c95bd031b3c5f8c8e959b8dc6
salman0167 <salmanlatheef49@gmail.com>
2023-07-05 18:23:48 +0530

    third commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import imag from '../assets/download.jpg'
+import './Home.css'
+import imag from '../assets/spotify.png'
 
 const Home = () => {
   return (
-    <div className="nav">
-    <div className="img"></div>
-    <img src={imag} alt="" />
-    <div className="a">premium</div>
-    <div className="b">Help</div>
-    <div className="c">Download</div>
-    <Link to={"/signup"}>
+    <>
+    <div className='contai'>
+      <div className='nav'>
+      <div className='image'>
+        <img src={imag} alt="" />
+      </div>
+      <p>Spotify</p>
+      <div className='a'>Premium</div>
+      <div className='b'>Help</div>
+      <div className='c'>Download</div>
+      <Link to={"/signup"}>
         <div className='d'>Sign Up</div>
-    </Link>
-    <Link to={"/login"}>
-        <div className='e'>Log In</div>  
-    </Link>
-    </div>
+      </Link>
+      <Link to={"/login"}>
+        <div className='e'>Log In</div>
+      </Link>
+    </div><div className="container">
+        <div className='sen1'>Music for Eveyone</div>
+        <div className='sen2'>Millions of songs.No credit card needed</div>
+        <button className='but'>GET SPOTIFY FREE
+        </button>
+      </div>
+      </div></>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <><div className='containe'>
+      <div className='contain'>
+      <div className='s1'>Login</div>
+      <form>
+        <label htmlFor="" className='em'>Email:</label>
+        <input type="email" placeholder='Enter email' className='emai' />
+        <label htmlFor="" className='pas'>Password:</label>
+        <input type="password" placeholder='Enter password' className='pass' />
+        <label className='s3'><input type="checkbox" className='s2' />Show Password</label>
+        <input type="submit" name="submit" value="SIGN IN" className="signin"></input>
+        <div className='for'>Forgot <a href="" className='user'> Username/Password</a></div>
+        <div className='don'>Don't have an account <a href="Signup" className='sig'> Sign up</a></div>
+      </form>
+    </div></div></>
   )
 }
 
